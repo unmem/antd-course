bd4a40ac87d8c80ed1363322814be6df6cc2bfc4
unmem <unmem@outlook.com>
2019-08-15 09:53:13 +0800

    Add menu into sidebar

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,7 +1,9 @@
 import { Component } from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu, Icon } from 'antd'
 
 const { Header, Footer, Sider, Content } = Layout
+
+const SubMenu = Menu.SubMenu
 
 class BasicLayout extends Component {
   constructor(props) {
@@ -11,8 +13,33 @@ class BasicLayout extends Component {
   render() {
     return (
       <Layout>
-        <Sider width={256} style={{ minHeight: '100vh', color: 'white' }}>
-          Sider
+        <Sider width={256} style={{ minHeight: '100vh' }}>
+          <div
+            style={{
+              height: '32px',
+              background: 'rgba(255,255,255,.2)',
+              margin: '16px',
+            }}
+          />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Helloworld</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="dashboard" />
+                  <span>Dashboard</span>
+                </span>
+              }
+            >
+              <Menu.Item key="2">分析页</Menu.Item>
+              <Menu.Item key="3">监控页</Menu.Item>
+              <Menu.Item key="4">工作台</Menu.Item>
+            </SubMenu>
+          </Menu>
         </Sider>
         <Layout>
           <Header
